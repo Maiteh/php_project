5a70ed6f2cb539854fb743128831b07763393b5f
Maiteh <maiteh_m@hotmail.com>
2014-04-04 22:29:02 +0200

    include footer toegevoegd

---
+++ include_footer.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title></title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-responsive.css">
+	<link rel="stylesheet" href="css/custom.css">
+	<script src="js/respond.js"></script>
+
+</head>
+<body>
+	
+	<footer>
+		<p class="text-center">Restaurant Application - 2014 | All rights reserved	</p>
+	</footer>
+
+</body>
+</html>
