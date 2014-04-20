c71b62bc2db3e244ee13a7fb27bf53f6fd3a1e36
GY22 <yipgrace.gy@gmail.com>
2014-04-20 08:23:35 +0200

    Dubbele code is verwijderd

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -30,90 +29,5 @@
 	
 </body>
 <html>
-=======
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Just in time</title>
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-	<!-- <link rel="stylesheet" href="css/bootstrap-min.css">   -->
-	<link rel="stylesheet" href="css/bootstrap-responsive.css">
-	<link rel="stylesheet" href="css/media_queries.css">
-	<link rel="stylesheet" href="css/custom.css">
-	<!-- <script src="js/respond.js"></script> -->
 
-   <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
-<body>
-	
-	<div class="container">
-
-	<!-- 
-		Extra small devices Phones (<768px)	
-	    Small devices Tablets (≥768px)	 
-	    Medium devices Desktops (≥992px)	 
-	    Large devices Desktops (≥1200px) 
-	-->
-
-		<!-- row 1 -->
-		<header class="row">
-			<div class="col-lg-12 col-lg-offset-1">
-				<img id="logo" class="img-responsive" src="images/logo.png" alt="logo">
-			</div>
-		</header>
-		<!-- end row 1 -->
-
-		<!-- row 2 -->
-			<section id="buttons" class="row">
-				<div class="col-lg-5 col-lg-offset-2">
-					<a href="register.php"><input class="img-responsive" type="image" src="images/btnImageR.png"></a> 
-					<!-- <div class="well">test 1</div>  -->
-				</div>
-				<div class="col-lg-5">
-					<a href="login.php"><input  class="img-responsive" type="image" src="images/btnImageL.png"></a>
-				 <!-- 	<div class="well">test 1</div>  -->
-				</div>
-			</section>
-		<!-- end row 2 -->
-
-		<?php include("include_footer.php");?>	
-
-	</div><!-- end container -->
-
-<!-- javascript -->
-	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
-	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-<!-- end javascript -->
-
-</body>
-</html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> FETCH_HEAD
 
--- login.php
+++ login.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 	
 	include_once("classes/User.class.php");
@@ -42,48 +41,3 @@
 </body>
 <html>
 
-=======
-<?php 
-
-	
-
-
-
-
-?><!doctype html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Just in time | Login</title>
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/bootstrap-responsive.css">
-	<link rel="stylesheet" href="css/custom.css">
-	<script src="js/respond.js"></script>
-
-
-</head>
-<body>
-	
-	<div id="container">
-		<!-- row 1 -->
-		
-		<!-- end row 1 -->
-
-		<!-- row 2 -->
-
-		<!-- end row 2 -->
-
-
-		<?php include("include_footer.php");?>	
-
-	</div><!-- end container -->
-
-<!-- javascript -->
-	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-<!-- end javascript -->
-
-</body>
-</html>
->>>>>>> FETCH_HEAD
