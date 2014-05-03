d72429c7bfac26e6d08c7cfa5b413230c97c4a07
Maiteh <maiteh_m@hotmail.com>
2014-05-03 17:26:59 +0200

    class verde

--- classes/User.class.php
+++ classes/User.class.php
@@ -14,22 +14,51 @@
 
 		public function __set($p_sProperty, $p_vValue)
 		{
-			switch ($p_sProperty) {
-
+			switch ($p_sProperty) 
+			{
+				
 				case 'Email':
-				$this->m_sEmail = $p_vValue; 
-				break;
-
+					if(!empty($p_vValue)){
+					$this->m_sEmail = $p_vValue;
+					}else{
+						$this->error["errorEmail"] = "Fill in an e-mail adress";
+					}
+					break;
+				
 				case 'Password':
-				//check to see if psw is at least 5 characters long
-				if(strlen($p_vValue) < 5)
-				{
-					throw new Exception ("Password must be at least 5 characters long");//een error genereren
-				}
+					if(strlen($p_vValue) < 5)
+		 			{
+		 				$this->error['errorPassword']="Choose a password of atleast 5 characters";
+		 			}
+					else
+		 			{
+		 				$salt = "IMVYJFDFGDSRT546564FYFGH";
+		 				$this->m_sPassword = md5($p_vValue.$salt);
+		 				break;
+		 			}
+		 		case 'Firstname':
+					if(!empty($p_vValue)){
+						$this->m_sFirstname = $p_vValue;
+					}else{
+						$this->error["errorFirstname"] = "Fill in your firstname";
+					}
+					break;
 
-				$this->m_sPassword = md5($p_vValue . $this->m_sSalt); //this verwijst naar het huidig object hier de variabele u
-				break;
-			}
+				case 'Lastname':
+					if(!empty($p_vValue)){
+					$this->m_sLastname = $p_vValue;
+					}else{
+						$this->error["errorLastname"] = "Fill in your lastname";
+					}
+					break;
+
+				case 'phone':
+					if(!empty($p_vValue)){
+					$this->m_sPhone = $p_vValue;
+					}else{
+						$this->error["errorPhone"] = "Fill in your phonenumber.";
+					}
+					break;
 		}
 
 		public function __get($p_sProperty)
